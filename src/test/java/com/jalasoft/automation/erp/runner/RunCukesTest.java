adf73d8d5ec664958ce396ea4154f80568d68f67
henry benito <henry.benito@jalasoft.com>
2016-04-04 18:30:27 -0400

    change tag in order to test other features for supervised employees

--- src/test/java/com/jalasoft/automation/erp/runner/RunCukesTest.java
+++ src/test/java/com/jalasoft/automation/erp/runner/RunCukesTest.java
@@ -14,7 +14,7 @@ import java.rmi.RemoteException;
  */
 
 @CucumberOptions(
-        tags = {"@ReadEmployeeInformation,@EditEmployeeInformation"},
+        tags = {"@EditEmployeeInformation"},
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
         glue = {"com.jalasoft.automation.erp.steps"},
         features = {"src/test/resources/com/jalasoft/automation/erp/features/"},

0b3c78ceda878a25650ebcab0b098ee0edd6d979
henry benito <henry.benito@jalasoft.com>
2016-05-24 14:54:49 -0400

    delete selog references and update abstract method on engineering class

--- src/main/java/com/jalasoft/automation/erp/objects/hhrr/employee/EngInfoEmployee.java
+++ src/main/java/com/jalasoft/automation/erp/objects/hhrr/employee/EngInfoEmployee.java
@@ -44,105 +44,106 @@ public class EngInfoEmployee extends OdooObject {
         this.nextEvalDate = aux.nextEvalDate;
     }
 
-    public boolean contains(boolean shouldContain, EngInfoEmployee mainData) {
+    public boolean matchWith(boolean shouldContain, OdooObject inputData) {
+        EngInfoEmployee uiData = (EngInfoEmployee)inputData;
         if (this.department != null) {
-            if(this.department.equals(mainData.department) != shouldContain) {
+            if(this.department.equals(uiData.department) != shouldContain) {
                 logWarning("department", this.department);
                 return false;
             }
         }
         if (this.division != null) {
-            if(this.division.equals(mainData.division) != shouldContain) {
+            if(this.division.equals(uiData.division) != shouldContain) {
                 logWarning("division", this.division);
                 return false;
             }
         }
         if (this.jobTitle != null) {
-            if(this.jobTitle.equals(mainData.jobTitle) != shouldContain) {
+            if(this.jobTitle.equals(uiData.jobTitle) != shouldContain) {
                 logWarning("jobTitle", this.jobTitle);
                 return false;
             }
         }
         if (this.manager != null) {
-            if(this.manager.equals(mainData.manager) != shouldContain) {
+            if(this.manager.equals(uiData.manager) != shouldContain) {
                 logWarning("manager", this.manager);
                 return false;
             }
         }
         if (this.lead != null) {
-            if (this.lead.equals(mainData.lead) != shouldContain) {
+            if (this.lead.equals(uiData.lead) != shouldContain) {
                 logWarning("lead", this.lead);
                 return false;
             }
         }
         if (this.visible != null) {
-            if (this.visible.equals(mainData.visible) != shouldContain) {
+            if (this.visible.equals(uiData.visible) != shouldContain) {
                 logWarning("visible", this.visible);
                 return false;
             }
         }
         if (this.billable != null) {
-            if (this.billable.equals(mainData.billable) != shouldContain) {
+            if (this.billable.equals(uiData.billable) != shouldContain) {
                 logWarning("billable", this.billable);
                 return false;
             }
         }
         if (this.shadowCategory != null) {
-            if (this.shadowCategory.equals(mainData.shadowCategory) != shouldContain) {
+            if (this.shadowCategory.equals(uiData.shadowCategory) != shouldContain) {
                 logWarning("shadowCategory", this.shadowCategory);
                 return false;
             }
         }
         if (this.notes != null) {
-            if (this.notes.equals(mainData.notes) != shouldContain) {
+            if (this.notes.equals(uiData.notes) != shouldContain) {
                 logWarning("notes", this.notes);
                 return false;
             }
         }
         if (this.commitment != null) {
-            if (this.commitment.equals(mainData.commitment) != shouldContain) {
+            if (this.commitment.equals(uiData.commitment) != shouldContain) {
                 logWarning("commitment", this.commitment);
                 return false;
             }
         }
         if (this.expDateCommit != null) {
-            if (this.expDateCommit.equals(mainData.expDateCommit) != shouldContain) {
+            if (this.expDateCommit.equals(uiData.expDateCommit) != shouldContain) {
                 logWarning("expDateCommit", this.expDateCommit);
                 return false;
             }
         }
         if (this.active != null) {
-            if (this.active.equals(mainData.active) != shouldContain) {
+            if (this.active.equals(uiData.active) != shouldContain) {
                 logWarning("active", this.active);
                 return false;
             }
         }
         if (this.consultant != null) {
-            if (this.consultant.equals(mainData.consultant) != shouldContain) {
+            if (this.consultant.equals(uiData.consultant) != shouldContain) {
                 logWarning("consultant", this.consultant);
                 return false;
             }
         }
         if (this.jce != null) {
-            if (this.jce.equals(mainData.jce) != shouldContain) {
+            if (this.jce.equals(uiData.jce) != shouldContain) {
                 logWarning("jce", this.jce);
                 return false;
             }
         }
         if (this.reason != null) {
-            if (this.reason.equals(mainData.reason) != shouldContain) {
+            if (this.reason.equals(uiData.reason) != shouldContain) {
                 logWarning("reason", this.reason);
                 return false;
             }
         }
         if (this.lastEvalDate != null) {
-            if (this.lastEvalDate.equals(mainData.lastEvalDate) != shouldContain) {
+            if (this.lastEvalDate.equals(uiData.lastEvalDate) != shouldContain) {
                 logWarning("lastEvalDate", this.lastEvalDate);
                 return false;
             }
         }
         if (this.nextEvalDate != null) {
-            if (this.nextEvalDate.equals(mainData.nextEvalDate) != shouldContain) {
+            if (this.nextEvalDate.equals(uiData.nextEvalDate) != shouldContain) {
                 logWarning("nextEvalDate", this.nextEvalDate);
                 return false;
             }
